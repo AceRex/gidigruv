17455ddd917400142fce1352619bf4f2685db9cf
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2020-10-30 17:24:39 +0100

    ght

--- gidi-gruv/src/index.js
+++ gidi-gruv/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, IndexRoute, Switch } from "react-router-dom";
-import Root from "./App";
+import App from "./App";
 import AboutUs from './About'
 import SED from './showEventDetails/SED'
 import SignIn from './Sign/SignIn'
@@ -15,18 +15,12 @@ import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
   <Router>
     <Switch>
-      <Route path="/" exact component={Root} />
+      <Route path="/" exact component={App} />
       <Route path="/about" component={AboutUs} />      
       <Route path="/signin" component={SignIn} />
       <Route path="/register" component={Register} />
       <Route path="/showeventdetails" component={SED} />
       <Route path="/dashboard" component={Dashboard} />
-
-
-
-
-
-
     </Switch>
   </Router>,
   document.getElementById("root")
